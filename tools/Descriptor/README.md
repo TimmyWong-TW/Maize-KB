ba69cff1133cbc015a503de14dfed8e584c6c222
Timothy Wong <tim@timmywong.tw>
2024-04-10 02:08:09 +0800

    Add 1164 overviews. Remove 18 empty transcripts.

--- tools/Descriptor/README.md
+++ tools/Descriptor/README.md
@@ -10,42 +10,8 @@ cd tools
 docker compose --rm -d descriptor
 ```
 
-## Sample output
->**標題：順服的領袖**
->
->**摘要：**
->
->本篇講道探討了領袖的順服品格，強調領袖必須順服聖靈的帶領、摩西的教導和同工的印證。講道指出，順服的領袖是神所興起的，他們將帶領神的子民進入豐盛的生命。
->
->**大綱：**
->
->1. **引言**
->    - 宣布結束時間
->    - 提醒填寫服事單
->    - 強調服事的價值
->    - 說明小組的重要性
->
->2. **約書亞的順服**
->    - 約書亞從年輕時就服侍摩西
->    - 約書亞一生忠心服事，但仍有未完成的使命
->    - 團隊合作的重要性
->
->3. **順服聖靈的帶領**
->    - 聖靈與情慾相爭
->    - 判斷順服聖靈的標準
->    - 巴蘭的例子
->
->4. **順服摩西的教導**
->    - 摩西將約書亞帶到神面前
->    - 順服權柄的重要性
->    - 可拉的叛亂
->
->5. **順服同工的印證**
->    - 約書亞需要大祭司的印證
->    - 肢體生活的價值
->    - 兩個支派的例子
->
->6. **結論**
->    - 呼籲加入小組
->    - 禱告，求神興起順服的領袖
->    - 讚美神，並奉主耶穌的名禱告
+## Exceptions
+
+Some content could not pass the moderation of Google AI, or otherwise causes empty output.
+
+Refer to `overview/error.log` for files that requires manual actions. Microsoft Copilot or another generative AI tool may be used manually.
--- tools/Descriptor/describe.mjs
+++ tools/Descriptor/describe.mjs
@@ -3,14 +3,28 @@ import { glob } from 'glob';
 import path from 'path';
 import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from '@google/generative-ai';
 
-const RPM = 15, requestInterval = 6e4 / RPM, TPM = 32000;
+let canceled = false, cancelTimeout = () => { };
+function cancel() {
+    canceled = true;
+    cancelTimeout();
+}
+process.on('SIGINT', cancel);
+process.on('SIGTERM', cancel);
+
+const RPM = 15, requestInterval = 6e4 / RPM, TPM = 32000, timeouts = [];
 let lastRequest = Date.now() - requestInterval;
 let accumulatedTokens = 0, resetTokens, lastTokenReset;
 
 async function describe(text) {
     const interval = Date.now() - lastRequest;
     if (interval < requestInterval) {
-        await new Promise(resolve => setTimeout(resolve, requestInterval - interval));
+        await new Promise((resolve, reject) => {
+            const timeout = setTimeout(resolve, requestInterval - interval);
+            cancelTimeout = () => {
+                clearTimeout(timeout);
+                reject('Canceled');
+            };
+        });
     }
     if (!resetTokens) {
         resetTokens = setInterval(() => {
@@ -36,7 +50,13 @@ async function describe(text) {
     })), prompt = `The text below is transcribed from Taiwan Mandarin, and may contain speech recognition errors due to homophones or otherwise that require correction, especially for names in the Bible. Generate a short title, a summary and an outline in Traditional Chinese after correction. Address the author as "講員" instead of "作者".\n"""\n${text}\n"""`;
     const tokenCount = await model.countTokens(prompt);
     if (accumulatedTokens + tokenCount > TPM) {
-        await new Promise(resolve => setTimeout(resolve, lastTokenReset + 6e4));
+        await new Promise((resolve, reject) => {
+            const timeout = setTimeout(resolve, lastTokenReset + 6e4 - Date.now());
+            cancelTimeout = () => {
+                clearTimeout(timeout);
+                reject('Canceled');
+            };
+        });
     }
     const result = await model.generateContent({
         contents: [{ role: 'user', parts: [{ text: prompt },] }],
@@ -48,31 +68,45 @@ async function describe(text) {
     return result.response.text();
 }
 
-let canceled = false;
-process.on('SIGINT', () => canceled = true);
-process.on('SIGTERM', () => canceled = true);
-
+const skip = (await fs.readFile('/data/overview/error.log', 'utf-8')).split('\n');
 for (const tsv of glob.sync('/data/tsv/**/*.tsv')) {
     if (canceled) {
         console.error("Canceled");
-        break;
+        process.exit(0);
     }
     const md = path.join('/data/overview', path.relative('/data/tsv', tsv).replace(/.tsv$/, '.md'));
+    if (skip.find(s => s === tsv || s === md)) {
+        continue;
+    }
     await fs.mkdir(path.dirname(md), { recursive: true });
     try {
         await fs.access(md, fs.constants.W_OK);
         // skip existing
     } catch (e) {
         if (e.errno === -2) {
-            const tmp = md + '~';
-            await fs.writeFile(tmp, await describe(
-                (await fs.readFile(tsv, 'utf-8'))
-                    .split('\n')
-                    .map(l => l.split('\t')[2])
-                    .join('\n')
-            ));
-            await fs.rename(tmp, md);
-            console.log(md);
+            const tmp = md + '~', transcript = (await fs.readFile(tsv, 'utf-8'))
+                .split('\n').slice(1)
+                .map(l => l.split('\t')[2])
+                .join('\n');
+            if (transcript.length > 0) {
+                let overview;
+                try {
+                    overview = await describe(transcript);
+                } catch (e) {
+                    console.error(e);
+                    console.error(tsv);
+                    await fs.appendFile('/data/overview/error.log', tsv + '\n');
+                    continue;
+                }
+                if (overview?.length > 0) {
+                    await fs.writeFile(tmp, overview);
+                    await fs.rename(tmp, md);
+                } else {
+                    console.error(md);
+                    await fs.appendFile('/data/overview/error.log', md + '\n');
+                    continue;
+                }
+            }
         } else {
             throw e; // could not determine existence
         }
