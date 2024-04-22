ec2042c83d5d29dae9b4f2b865a130c7fad7da1b
Timothy Wong <tim@timmywong.tw>
2024-04-23 05:55:38 +0800

    Convert Chinese characters

--- tools/ChineseConverter/convert.mjs
+++ tools/ChineseConverter/convert.mjs
@@ -1,17 +1,16 @@
 import { promises as fs } from 'fs';
-import OpenCC from 'opencc';
+import { traditionalize } from 'hanzi-tools';
 import { glob } from 'glob';
-const converter = new OpenCC('s2tw.json');
 await Promise.all(glob.sync('/data/tsv/**/*.tsv').map(async tsv => {
-    const tmp = tsv + '~';
+    const tmp = tsv + '~', lines = (await fs.readFile(tsv, 'utf-8')).split('\n');
     await fs.writeFile(tmp, [
-        'start\tend\ttext',
-        ...await Promise.all((await fs.readFile(tsv, 'utf-8'))
-            .split('\n').slice(1)
-            .map(async l => {
-                const [start, end, text] = l.split('\t');
-                return `${start}\t${end}\t${await converter.convertPromise(text)}`;
-            }))
+        lines[0],
+        ...lines.slice(1).map(l => {
+            const [start, end, text] = l.split('\t');
+            return `${start}\t${end}\t${traditionalize(text)
+                .replace(/(禰)|(麵對)|(裡路)|((?<=不單?)隻)|((?<=[水泳].*)遊)|((?<=只)準|(?<![水不看瞄對標])準(?=[他了許我你她祂牠它]))|(面嚮)/g, (_, a, b, c, d, e, f, g) =>
+                    a ? '祢' : b ? '面對' : c ? '里路' : d ? '只' : e ? '游' : f ? '准' : g ? '面向' : _)}`;
+        })
     ].join('\n'));
     await fs.unlink(tsv);
     await fs.rename(tmp, tsv);
