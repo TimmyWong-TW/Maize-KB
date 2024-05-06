84eec144d2ab6a639d4e7ccd154dadfa73ada8ca
Timothy Wong <tim@timmywong.tw>
2024-05-07 03:56:30 +0800

    Evaluate MADLAD-400

---
+++ tools/Translator/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+docker build --target src -t translator:src -f Translator/Dockerfile .
+# docker rm translator-build
+docker run --name translator-build --gpus all translator:src
+docker commit translator-build translator:build
+# docker rm translator-build
+docker compose build translator
+# docker rmi translator:src translator:build
