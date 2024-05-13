adedf2d8a36447cfc41c3a6078d3527f4d1531fa
Timothy Wong <tim@timmywong.tw>
2024-05-14 05:51:41 +0800

    Evaluate LLaMa 3 TAIDE

--- playground/embedding/main.py
+++ playground/embedding/main.py
@@ -1,14 +1,13 @@
-from flask import Flask, request, jsonify
-from sentence_transformers import SentenceTransformer
-from sklearn.preprocessing import normalize
+from flask import Flask, request, Response
+from FlagEmbedding import BGEM3FlagModel
 import base64
 import json
 import struct
 
-embedding_model_name = 'infgrad/stella-mrl-large-zh-v3.5-1792d'
+embedding_model_name = 'BAAI/bge-m3'
 app = Flask(__name__)
 app.json.sort_keys = False
-embedder = SentenceTransformer(embedding_model_name)
+embedder = BGEM3FlagModel(embedding_model_name, use_fp16=True)
 
 def round12(value):
     return round(value, 9 if value < 0 else 10)
@@ -17,34 +16,30 @@ def round12(value):
 def embed():
     input = request.json['input']
     is_base64 = request.json.get('encoding_format', 'float') == 'base64'
-    dimensions = request.json.get('dimensions', 1792)
-    trim = dimensions < 1792
-    inputs = input if isinstance(input, list) else [input]
-    # tokens = sum([len(embedder.tokenize(i)['input_ids']) for i in inputs])
-    tokens = sum(len(i) for i in inputs)
-    vectors = embedder.encode(
-        inputs,
-        normalize_embeddings=not trim
+    embeddings = embedder.encode(
+        input if isinstance(input, list) else [input],
+        batch_size=8,
+        max_length=8192,
+        return_colbert_vecs=True
     )
-    if trim:
-        vectors = normalize(vectors[:, :dimensions])
-    return jsonify({
-        'object': 'list',
-        'data': [
-            {
+    vectors = embeddings['dense_vecs'].tolist()
+    tokens = sum([len(v) for v in embeddings['colbert_vecs']])
+    def generate():
+        t = str(tokens)
+        yield '{"object":"list","data":['
+        sub = False
+        for i, vector in enumerate(vectors):
+            if sub:
+                yield ','
+            sub = True
+            yield json.dumps({
                 'object': 'embedding',
                 'embedding': base64.b64encode(struct.pack('f'*len(vector), *vector)).decode('utf-8')
                     if is_base64 else [round12(f) for f in vector],
                 'index': i
-            }
-            for i, vector in enumerate(vectors.astype(float))
-        ],
-        'model': embedding_model_name,
-        'usage': {
-            'prompt_tokens': tokens,
-            'total_tokens': tokens
-        }
-    })
+            }, ensure_ascii=False)
+        yield '],"model":' + json.dumps(embedding_model_name) + ',"usage":{"prompt_tokens":' + t + ',"total_tokens":' + t + '}}'
+    return Response(generate(), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
